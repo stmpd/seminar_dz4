b6df36362c92fb822a49e3dc91b56ca14521ec64
George.S <piligrimius@mail.ru>
2022-08-06 00:16:13 +0300

    Task_29 Completed[Reworked]

--- Task_29/Program.cs
+++ Task_29/Program.cs
@@ -18,14 +18,14 @@ int[] RandomArray( int arrayLength)
 
 void PrintArray( int[] array) 
 {
-    Console.Write("[");
+    Console.Write("(");
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i]);
         if (i != array.Length - 1)
         Console.Write(", ");
     }
-    Console.Write("]");
+    Console.Write(")");
 }
 
 PrintArray(array);
